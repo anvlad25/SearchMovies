937e8e5a18e0776ffa4f536af6cdc3143cf84b26
anvlad25 <82762246+anvlad25@users.noreply.github.com>
2021-09-13 16:55:20 +0300

    push

---
+++ app/src/main/java/com/example/searchmovies/model/service/MessagingService.kt
@@ -0,0 +1,75 @@
+package com.example.searchmovies.model.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.example.searchmovies.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MessagingService : FirebaseMessagingService() {
+
+    private val TAG = "Pushes"
+    private val NO_DATA = "no data!"
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        val customData = message.data[CUSTOM_FIELD]
+        Log.i(TAG, customData ?: NO_DATA)
+    }
+
+    private fun handleDataMessage(data: Map<String, String>) {
+        val title = data[PUSH_KEY_TITLE]
+        val message = data[PUSH_KEY_MESSAGE]
+
+        if (!title.isNullOrBlank() && !message.isNullOrBlank()) {
+            showNotification(title, message)
+        }
+    }
+
+    private fun showNotification(title: String, message: String) {
+        val notificationBuilder =
+            NotificationCompat.Builder(applicationContext, CHANNEL_ID).apply {
+                setSmallIcon(R.mipmap.ic_launcher)
+                setContentTitle(title)
+                setContentText(message)
+                priority = NotificationCompat.PRIORITY_DEFAULT
+            }
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val name = "Push-уведомления"
+        val descriptionText = "Channel description"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object {
+        private const val PUSH_KEY_TITLE = "title"
+        private const val PUSH_KEY_MESSAGE = "message"
+        private const val CUSTOM_FIELD = "custom"
+        private const val CHANNEL_ID = "channel_id"
+        private const val NOTIFICATION_ID = 37
+    }
+}
